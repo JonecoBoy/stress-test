ab48f7304541b92197a03ed71ef57e6d58b0e86a
Joneco <joneco@hotmail.com>
2024-07-06 20:01:04 -0300

    all outputs ok

--- cmd/test.go
+++ cmd/test.go
@@ -8,7 +8,6 @@ import (
 	"github.com/JonecoBoy/stress-test/appContext"
 	"github.com/JonecoBoy/stress-test/reporter"
 	"github.com/JonecoBoy/stress-test/requester"
-	"path/filepath"
 	"time"
 
 	"github.com/spf13/cobra"
@@ -19,6 +18,7 @@ var url string
 var requestNumber uint
 var concurrentRequests uint
 var quiet bool
+var addTimeStamp bool
 var outputPath string
 var outputType string
 
@@ -31,7 +31,7 @@ var testCmd = &cobra.Command{
 			-u --url: set target URL
 			-r --requests: set number of requests
 			-c --concurrency: set number of concurrent requests
-			-q --quiet: dont log requests status
+			-q --quiet: dont log requests statuses
 			Example:
 			stresser test --url http://localhost:8080/api/v1/test --requests 100 --concurrency 10
 `,
@@ -53,43 +53,31 @@ var testCmd = &cobra.Command{
 		req.Start(ctx)
 		ctx.TotalTime = time.Since(start)
 
-		rep.CliReport()
-
-		if reportFormat == "html" || reportFormat == "all" {
-			// Remove the extension from the filename and add .html
-			outputPath = filepath.Base(outputPath)
-			ext := filepath.Ext(outputPath)
-			outputPath = outputPath[0:len(outputPath)-len(ext)] + ".html"
-
-			err := rep.GenerateHTMLReport(outputPath)
-			if err != nil {
-				fmt.Println("Error generating HTML report:", err)
-			}
-		} else {
-			err := rep.LogToFile(outputPath, outputType)
-			if err != nil {
-				fmt.Println("Error writing report to file:", err)
-			}
-
+		report, err := rep.PrepareReport(outputType)
+		if err != nil {
+			fmt.Println("Error preparing report:", err)
 		}
+		rep.CliReport(report)
+
+		// Remove the extension from the filename and add .html
+
+		if outputPath == "" {
+			outputPath = "report-" + time.Now().Format("20060102150405")
+		}
+
+		err = rep.GenerateHTMLReport(outputPath)
+		if err != nil {
+			fmt.Println("Error generating HTML report:", err)
+		}
+
+		err = rep.LogToFile(report, outputPath, outputType)
+		if err != nil {
+			fmt.Println("Error writing report to file:", err)
+		}
+
 		return nil
 	},
 }
-
-//var reporterCmd = &cobra.Command{
-//	Use:   "report",
-//	Short: "Generate a report",
-//	Long:  `This command will generate a report in the specified format (txt or html).`,
-//	Run: func(cmd *cobra.Command, args []string) {
-//		ctx := &appContext.Context{
-//			URL:           url,
-//			TotalRequests: int(requestNumber),
-//			Concurrency:   int(concurrentRequests),
-//			StatusCodes:   make(map[int]int),
-//		}
-//
-//	},
-//}
 
 func init() {
 	rootCmd.AddCommand(testCmd)
@@ -98,7 +86,8 @@ func init() {
 	testCmd.PersistentFlags().StringVarP(&url, "url", "u", "", "set target URL (Required)")
 	testCmd.PersistentFlags().UintVarP(&requestNumber, "requests", "r", 100, "set number of total requests")
 	testCmd.PersistentFlags().UintVarP(&concurrentRequests, "concurrency", "c", 10, "set number of concurrent requests")
-	testCmd.PersistentFlags().StringVarP(&outputPath, "output", "o", "report.txt", "set output file for the report")
+	testCmd.PersistentFlags().StringVarP(&outputPath, "output", "o", "", "set output file for the report")
+	testCmd.PersistentFlags().BoolVarP(&addTimeStamp, "timestamp", "t", true, "add timestamp to the output file name")
 	testCmd.PersistentFlags().StringVarP(&outputType, "encode", "e", "", "set output format for the report (csv, json, toml, yaml)")
 	testCmd.PersistentFlags().StringVarP(&reportFormat, "format", "f", "stdout", "set report output format (stdout,txt or html)")
 	testCmd.PersistentFlags().BoolVarP(&quiet, "quiet", "q", false, "set quiet mode (won't print all response statuses)")
--- reporter/reporter.go
+++ reporter/reporter.go
@@ -4,14 +4,29 @@ import (
 	"bytes"
 	"encoding/csv"
 	"encoding/json"
+	"encoding/xml"
 	"fmt"
 	"github.com/BurntSushi/toml"
 	"github.com/JonecoBoy/stress-test/appContext"
 	"gopkg.in/yaml.v3"
 	"io/ioutil"
+	"path/filepath"
 	"strings"
 	"time"
 )
+
+type ErrorEntry struct {
+	Code  int `xml:"Code"`
+	Count int `xml:"Count"`
+}
+
+type Report struct {
+	TotalTimeSpent          string       `xml:"TotalTimeSpent" json:"Total time spent" csv:"Total time spent" yaml:"Total time spent" toml:"Total time spent"`
+	TotalRequests           int          `xml:"TotalRequests" json:"Total requests" csv:"Total requests" yaml:"Total requests" toml:"Total requests"`
+	AverageRequestTimeSpent string       `xml:"AverageRequestTimeSpent" json:"Average request time spent" csv:"Average request time spent" yaml:"Average request time spent" toml:"Average request time spent"`
+	SuccessfulRequests      int          `xml:"SuccessfulRequests" json:"Successful requests" csv:"Successful requests" yaml:"Successful requests" toml:"Successful requests"`
+	ErrorDistribution       []ErrorEntry `xml:"ErrorDistribution" json:"Errors Distribution" csv:"Errors Distribution" yaml:"Errors Distribution" toml:"Errors Distribution"`
+}
 
 type Reporter struct {
 	Context *appContext.Context
@@ -23,7 +38,8 @@ func NewReporter(ctx *appContext.Context) *Reporter {
 	}
 }
 
-func (r *Reporter) CliReport() {
+func (r *Reporter) PrepareReport(fileType string) (string, error) {
+	var err error
 	total := time.Duration(0)
 	for _, t := range r.Context.RequestTimes {
 		total += t
@@ -36,105 +52,146 @@ func (r *Reporter) CliReport() {
 		average = total / duration
 	}
 
-	fmt.Println("Total time spent:", r.Context.TotalTime)
-	fmt.Println("Total requests:", r.Context.TotalRequests)
-	fmt.Println("Average request time spent:", average)
-	fmt.Println("Successful requests:", r.Context.SuccessfulRequests)
-	//fmt.Println("Status code distribution:")
-	//for code, count := range r.Context.StatusCodes {
-	//	fmt.Printf("  %d: %d\n", code, count)
-	//}
-	// Create a map to hold the error distribution
-	errorDistribution := make(map[int]int)
-	for _, err := range r.Context.Errors {
-		// Increment the count for this error message
-		errorDistribution[err]++
-	}
-
-	fmt.Println("Error distribution:")
-	for errCode, count := range errorDistribution {
-		fmt.Printf("  %d: %d\n", errCode, count)
-	}
-}
-
-func (r *Reporter) LogToFile(filename string, fileType string) error {
-	// Prepare the report data
-	total := time.Duration(0)
-	for _, t := range r.Context.RequestTimes {
-		total += t
-	}
-	average := total / time.Duration(len(r.Context.RequestTimes))
-
 	percentages := make([]float64, len(r.Context.RequestTimes))
 	for i, t := range r.Context.RequestTimes {
 		percentages[i] = float64(t) / float64(r.Context.TotalTime) * 100
 	}
 
-	report := map[string]interface{}{
-		"Total time spent":           r.Context.TotalTime,
-		"Total requests":             r.Context.TotalRequests,
-		"Average request time spent": average,
-		"Successful requests":        r.Context.SuccessfulRequests,
-		"Error distribution":         r.Context.Errors,
-		"Requests Average time":      average,
+	// Create a slice to hold the error entries
+	var ErrorsDistribution []ErrorEntry
+
+	// Iterate over the errors and create an entry for each error
+	for _, err := range r.Context.Errors {
+		// Find if the error code already exists in the slice
+		found := false
+		for i, entry := range ErrorsDistribution {
+			if entry.Code == err {
+				// If the error code exists, increment the count
+				ErrorsDistribution[i].Count++
+				found = true
+				break
+			}
+		}
+
+		// If the error code does not exist in the slice, add a new entry
+		if !found {
+			ErrorsDistribution = append(ErrorsDistribution, ErrorEntry{Code: err, Count: 1})
+		}
 	}
+
+	report := Report{
+		TotalTimeSpent:          fmt.Sprintf("%v", r.Context.TotalTime),
+		TotalRequests:           r.Context.TotalRequests,
+		AverageRequestTimeSpent: fmt.Sprintf("%v", average),
+		SuccessfulRequests:      r.Context.SuccessfulRequests,
+		ErrorDistribution:       ErrorsDistribution,
+	}
+
+	// Flatten the errorDistribution map
+	var flatErrorDistribution []string
+	for errCode, count := range ErrorsDistribution {
+		flatErrorDistribution = append(flatErrorDistribution, fmt.Sprintf("%d: %d", errCode, count))
+	}
+
 	var data []byte
-	var err error
 
 	switch fileType {
 	case "json":
 		data, err = json.Marshal(report)
 		if err != nil {
-			return err
+			return "", err
 		}
 	case "yaml":
 		data, err = yaml.Marshal(report)
 		if err != nil {
-			return err
+			return "", err
 		}
 	case "toml":
+		//report["Errors Distribution"] = flatErrorDistributionStr
 		data, err = toml.Marshal(report)
 		if err != nil {
-			return err
+			return "", err
+		}
+	case "xml":
+		//report["Errors Distribution"] = flatErrorDistributionStr
+		data, err = xml.MarshalIndent(report, "", "  ")
+		if err != nil {
+			return "", err
 		}
 	case "csv":
 		b := &bytes.Buffer{}
 		w := csv.NewWriter(b)
-		for key, value := range report {
-			err := w.Write([]string{key, fmt.Sprintf("%v", value)})
+
+		// Write the non-map fields to the CSV
+		err := w.Write([]string{"Total time spent", report.TotalTimeSpent})
+		if err != nil {
+			return "", err
+		}
+		err = w.Write([]string{"Total requests", fmt.Sprintf("%d", report.TotalRequests)})
+		if err != nil {
+			return "", err
+		}
+		err = w.Write([]string{"Average request time spent", report.AverageRequestTimeSpent})
+		if err != nil {
+			return "", err
+		}
+		err = w.Write([]string{"Successful requests", fmt.Sprintf("%d", report.SuccessfulRequests)})
+		if err != nil {
+			return "", err
+		}
+
+		// Flatten the ErrorDistribution map and write it to the CSV
+		for errCode, count := range report.ErrorDistribution {
+			err = w.Write([]string{fmt.Sprintf("Error %d", errCode), fmt.Sprintf("%d", count)})
 			if err != nil {
-				return err
+				return "", err
 			}
 		}
+
 		w.Flush()
 		data = b.Bytes()
 	default:
-		data = []byte(fmt.Sprintf("Total time spent: %v\nTotal requests: %d\nSuccessful requests: %d\n", r.Context.TotalTime, r.Context.TotalRequests, r.Context.SuccessfulRequests))
+		report := fmt.Sprintf("Total time spent: %v\nTotal requests: %d\nSuccessful requests: %d\n",
+			r.Context.TotalTime, r.Context.TotalRequests, r.Context.SuccessfulRequests)
+
+		report += "Error distribution:\n"
+		for _, errorEntry := range ErrorsDistribution {
+			report += fmt.Sprintf("  %d: %d\n", errorEntry.Code, errorEntry.Count)
+		}
+
+		data = []byte(report)
 	}
 
+	//report := fmt.Sprintf("Total time spent: %v\nTotal requests: %d\nAverage request time spent: %v\nSuccessful requests: %d\n",
+	//	r.Context.TotalTime, r.Context.TotalRequests, average, r.Context.SuccessfulRequests)
+	//
+	//report += "Error distribution:\n"
+	//for errCode, count := range errorDistribution {
+	//	report += fmt.Sprintf("  %d: %d\n", errCode, count)
+	//}
+
+	return string(data), nil
+}
+
+func (r *Reporter) CliReport(report string) {
+	fmt.Println(report)
+}
+
+func (r *Reporter) LogToFile(data string, filename string, fileType string) error {
 	// If filename doesn't have an extension, add it
 	if !strings.Contains(filename, ".") {
 		filename = fmt.Sprintf("%s.%s", filename, fileType)
 	}
 
 	// Write data to the file
-	return ioutil.WriteFile(filename, data, 0644)
+	return ioutil.WriteFile(filename, []byte(data), 0644)
 
-	//// Add the error distribution
-	//data += "Error distribution:\n"
-	//errorDistribution := make(map[int]int)
-	//for _, err := range r.Context.Errors {
-	//	errorDistribution[err]++
-	//}
-	//for errCode, count := range errorDistribution {
-	//	data += fmt.Sprintf("  %d: %d\n", errCode, count)
-	//}
-	//
-	//// Write data to the file
-	//return ioutil.WriteFile(filename, []byte(data), 0644)
 }
 
 func (r *Reporter) GenerateHTMLReport(filename string) error {
+	filename = filepath.Base(filename)
+	ext := filepath.Ext(filename)
+	filename = filename[0:len(filename)-len(ext)] + ".html"
 	percentages := make([]float64, len(r.Context.RequestTimes))
 	for i, t := range r.Context.RequestTimes {
 		percentages[i] = float64(t) / float64(r.Context.TotalTime) * 100
@@ -143,7 +200,7 @@ func (r *Reporter) GenerateHTMLReport(filename string) error {
 	for _, t := range r.Context.RequestTimes {
 		total += t
 	}
-	//average := total / time.Duration(len(r.Context.RequestTimes))
+	average := total / time.Duration(len(r.Context.RequestTimes))
 
 	// Prepare the HTML report
 	html := "<html><head>"
@@ -155,6 +212,7 @@ func (r *Reporter) GenerateHTMLReport(filename string) error {
         drawErrorDistributionChart();
         drawSuccessErrorChart();
         drawRequestTimesChart();
+        drawAllRequestTimesChart();
     }
 
     function drawErrorDistributionChart() {
@@ -248,12 +306,35 @@ func (r *Reporter) GenerateHTMLReport(filename string) error {
         var chart = new google.visualization.BarChart(document.getElementById('requestTimesChart'));
 
         chart.draw(data, options);
+    }`
+	html += `function drawAllRequestTimesChart() {
+        var data = new google.visualization.DataTable();
+        data.addColumn('string', 'Request Time');
+        data.addColumn('number', 'Time (ms)');
+        data.addColumn('number', 'Average Time (ms)');`
+
+	// Add the request times
+	for i, requestTime := range r.Context.RequestTimes {
+		html += fmt.Sprintf("data.addRow(['Request %d', %d, %f]);", i+1, requestTime.Milliseconds(), average.Seconds()*1000)
+	}
+
+	html += `
+        var options = {
+            title: 'All Request Times',
+            seriesType: 'bars',
+            series: {1: {type: 'line'}}
+        };
+
+        var chart = new google.visualization.ComboChart(document.getElementById('allRequestTimesChart'));
+        chart.draw(data, options);
     }
-    </script>
+	</script>
     </head><body>`
-	html += `<div id="errorDistributionChart" style="width: 900px; height: 500px;"></div>`
+
 	html += `<div id="successErrorChart" style="width: 900px; height: 500px;"></div>`
+	html += `<div id="errorDistributionChart" style="width: 900px; height: 500px;"></div>`
 	html += `<div id="requestTimesChart" style="width: 900px; height: 500px;"></div>`
+	html += `<div id="allRequestTimesChart" style="width: 900px; height: 500px;"></div>`
 	html += "</body></html>"
 
 	// Write HTML to the file
